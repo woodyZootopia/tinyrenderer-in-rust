3523d0df34c9b486b171393246e39180dcad9431
woodyZootopia <piequalsabout314159@gmail.com>
2022-03-18 14:41:49 +0900

    Lesson1, Second Attempt

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,22 @@ pub fn render() {
     let image_height = (image_width as f32 / aspect_ratio) as usize;
     let mut image = vec![vec![Color::new(0., 0., 0.); image_width]; image_height];
 
-    line(90, 10, 70, 80, &mut image, Color::new(1., 1., 1.));
+    // line(90, 10, 70, 80, &mut image, Color::new(1., 1., 1.));
+    let white = Color::new(1., 1., 1.);
+    let red = Color::new(1., 0., 0.);
+    line(13, 20, 80, 40, &mut image, white);
+    line(20, 13, 40, 80, &mut image, red);
+    line(80, 40, 13, 20, &mut image, red);
 
     // output rendering result
     draw_print(image);
 }
 
-fn line(h0: isize, w0: isize, h1: isize, w1: isize, image: &mut Vec<Vec<Color>>, color: Color) {
-    let num_point = 100;
-    for i in 0..num_point {
-        let t = i as f32 / num_point as f32;
-        let (h, w) = (
-            h0 as f32 + (h1 - h0) as f32 * t,
-            w0 as f32 + (w1 - w0) as f32 * t,
-        );
-        image[h as usize][w as usize] = color;
+fn line(x0: isize, y0: isize, x1: isize, y1: isize, image: &mut Vec<Vec<Color>>, color: Color) {
+    let (y0, y1) = (image.len() as isize - y0, image.len() as isize - y1);
+    for x in x0..x1 {
+        let t = (x - x0) as f32 / (x1 - x0) as f32;
+        let y = y0 as f32 + (y1 - y0) as f32 * t;
+        image[y as usize][x as usize] = color;
     }
 }
